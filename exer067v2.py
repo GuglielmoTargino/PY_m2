b05f5f7d342c516b57819db983fb5a8f8214f624
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-02-23 21:40:34 -0300

    f_print
    Alteração para o novo formato de imprimir com o f_print na linha 7.

--- exer067v2.py
+++ exer067v2.py
@@ -1,1 +1,9 @@
-#programa de tabuada com laço while e ponto de parada breck
+#programa de tabuada com laço while e ponto de parada break
+while True:
+    tb=int(input('Digite a tabuada. Ou um número negativo para sair_'))
+    if tb<0:
+        break
+    for c in range(1,11):
+        print(f'{tb} x {c} = {tb*c}')
+
+print('Número negativo digitado. Fim tabuada')
