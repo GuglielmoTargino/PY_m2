834083301dce15374b890685d939784213eb8b4e
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-02-24 19:54:08 -0300

    adição de comentários apenas
    Adição de comentários apenas.

--- exer096.py
+++ exer096.py
@@ -4,16 +4,16 @@ ctdora=ctdoro=ctdorm=ctdorg=0
 while True:
     idade=int(input('digite a idade em anos_'))
     sexo=str(input('digite o sexo [M/F]')).strip().upper()[0]
-    ctdorg+=1
+    ctdorg+=1 # contador geral de pessoas inseridas
 
-    if idade>18:
+    if idade>18: # linha para contar os maiores de idade
         ctdora+=1
 
-    if sexo=="M":
+    if sexo=="M": #linha para incrementar o contador masculino
         ctdoro+=1
-    elif idade<20:
+    elif idade<20: #linha para incrementar o conyador feminino menor de 18 anos de idade
         ctdorm+=1
-    bda=str(input('deseja continuar?_[S/N]')).strip().upper()[0]
+    bda=str(input('deseja continuar?_[S/N]')).strip().upper()[0] # #linha para flag de parada
 
     if bda=='N':
         break
