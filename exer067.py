a7a7e19a57adde90f39662877928d41264c46cca
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-02-23 21:08:31 -0300

    exer067
    Exercicio de tabuada com laço de repetição while.

---
+++ exer067.py
@@ -0,0 +1,21 @@
+
+tb=int(input('Digite qual tabuada vc quer saber_'))
+ctor=0  # contador iiciado em zero
+
+while tb>0: 
+
+    while ctor<10:
+        ctor+=1
+        prod=tb*ctor
+        print('{} x {} = {}'.format(tb,ctor,prod))
+    tb=int(input('Digite outra tabuada? Ou digite um numero negativo para sair_'))
+    ctor=0 #linha para zerar o contador e começar de novo o loop
+print('Fim Tabuada')
+
+
+
+
+
+
+
+
