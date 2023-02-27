7d07f762ea19897e3c7c76996a0f6f4aff54925a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-02-27 16:57:42 -0300

    adicao comentarios
    Adição de comentários explicando as variáveis apenas .

--- exerc070.py
+++ exerc070.py
@@ -1,25 +1,30 @@
 #programa para analisar dados de uma compra.
 maior=menor=ctdr=soma=qtd=0
 nmprod=' '
+# maior guarda maior valor
+#menor guarda menor valor
+#ctdr contador para entrada de produtos
+#soma guarda soma dos produtos
+#qtd guarda quantos produtos custaram mais de R$1000.00 
 
 while True:
-    bda=' '
+    bda=' '# bandeira recebe SIM ou NÃo do usuário para continua
     prod=str(input('Digite o produto_')).strip()
     pco=float(input('Digite o perço do produto R$_'))
     soma+=pco
     ctdr+=1
-    if ctdr==1:
+    if ctdr==1:# se o valor for o inicial. seta menor com o valor digita.
         menor=pco
         nmprod=prod
-    if pco>maior:
+    if pco>maior:# armazena o maior valor
         maior=pco
-    if pco<menor:
+    if pco<menor:#armazena o menor valor
         menor=pco
         nmprod=prod
     if pco>1000:
         qtd+=1
 
-    while bda not in 'SN':
+    while bda not in 'SN': # filtro linha para contra digitação errada do usuario
         bda=str(input('Deseja continuar? [S/N]')).strip().upper()[0]
     
     if bda=='N':
