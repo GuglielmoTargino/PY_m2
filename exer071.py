9ed3d5d7479bdc5ec0f6fdb77bb97e06f5ca791a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-02-28 16:47:01 -0300

    termino exer.
    Finalização do exercício com sucesso.

--- exer071.py
+++ exer071.py
@@ -1,3 +1,35 @@
 # programa para simular uma contagem de notas para saque em um ATM
 
 saque=int(input('Digite quanto vc que sacar_ R$'))
+ctdr=0
+while saque>=50:
+    saque-=50
+    ctdr+=1
+print(f'{ctdr} notas de R$50.00.')
+ctdr=0
+
+while saque>=20:
+    saque-=20
+    ctdr+=1
+print(f'{ctdr} notas de R$20.00.')
+ctdr=0
+
+while saque>=10:
+    saque-=10
+    ctdr+=1
+
+print(f'{ctdr} notas de R$10,00.')
+ctdr=0
+while saque>=5:
+    saque-=5
+    ctdr+=1
+
+print(f'{ctdr} notas de R$5,00')
+ctdr=0
+
+while saque>=1:
+    saque-=1
+    ctdr+=1
+
+print(f'{ctdr} notas de R$1,00.')
+
